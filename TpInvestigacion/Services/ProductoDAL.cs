452490e36592105488d66b2c37cbb42d57a1cf5f
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-10-15 16:45:46 -0300

    Creado servicio ProductoDAL

---
+++ TpInvestigacion/Services/ProductoDAL.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TpInvestigacion.Models;
+
+namespace TpInvestigacion.Services
+{
+	public class ProductoDAL
+	{
+		private readonly IMongoDatabase _database;
+
+		public ProductoDAL()
+		{
+			_database = Conectar();
+		}
+
+		public IEnumerable<Producto> Todos()
+		{
+			var productos = _database.GetCollection<Producto>("productos").Find(new BsonDocument()).ToListAsync();
+			return productos.Result;
+		}
+
+		private IMongoDatabase Conectar()
+		{
+			var cliente = new MongoClient("mongodb://localhost:27017");
+			var database = cliente.GetDatabase("mitienda");
+			return database;
+		}
+	}
+}
