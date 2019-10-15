2bcc3860685da12aefc675b4b733eca2d385f812
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-10-15 16:45:58 -0300

    Creado Controller de Productos

---
+++ TpInvestigacion/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TpInvestigacion.Models;
+using TpInvestigacion.Services;
+
+namespace TpInvestigacion.Controllers
+{
+	[Route("api/[controller]")]
+	public class ProductosController : Controller
+    {
+		[HttpGet]
+		public IEnumerable<Producto> GetAll()
+		{
+			ProductoDAL dal = new ProductoDAL();
+			return dal.Todos();
+		}
+	}
+}
