f92e377cc78d4555e370ef49ebac0328f2464185
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-10-15 16:45:28 -0300

    Creada clase Producto

---
+++ TpInvestigacion/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TpInvestigacion.Models
+{
+	public class Producto
+	{
+		[BsonId]
+		public ObjectId id { get; set; }
+
+		[BsonElement ("nombre")]
+		public string Nombre { get; set; }
+
+		[BsonElement("cantidad")]
+		public int Cantidad { get; set; }
+
+		[BsonElement("precio")]
+		public int Precio { get; set; }
+	}
+}
